401e0a2c5d836fd84a8e2b8a55d4f835030eed60
Emanuele Longo <emanuele.longo@ynap.com>
2018-12-16 14:11:08 +0100

    save

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Alexa = require('ask-sdk');
-const lg = require('./LG')("192.168.1.109");
+const lg = require('./LG')("192.168.1.4");
 const skillBuilder = Alexa.SkillBuilders.standard();
 
 exports.handler = skillBuilder
